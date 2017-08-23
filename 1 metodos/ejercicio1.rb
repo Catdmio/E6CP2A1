4e8263d782eec26e283e1c36f4ed9d6ca8bae1bf
Catdmio <montecinos.catalina@gmail.com>
2017-08-23 20:32:32 -0300

    adding exercise one. Refactoring of ruby code

--- 1 metodos/ejercicio1.rb
+++ 1 metodos/ejercicio1.rb
@@ -2,12 +2,21 @@
 # hint: Debes utilizar 'rubocop'
 # y no obtener errores ni warnings.
 
+# def par(x)
+#   if x % 2 == 0
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+# puts par(2)
+# puts par(3)
+# puts par(4)
+# puts par(5)
+
 def par(x)
-  if x % 2 == 0
-    return true
-  else
-    return false
-  end
+	(x % 2).zero? ? true : false
 end
 
 puts par(2)
